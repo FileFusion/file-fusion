fe92f4493c19faaddba62ba4a25a0caad738073d
hackyo <137120918@qq.com>
2025-02-05 22:41:51 +0800

    file list

--- backend/src/main/java/com/github/filefusion/constant/FileAttribute.java
+++ backend/src/main/java/com/github/filefusion/constant/FileAttribute.java
@@ -10,7 +10,7 @@ public final class FileAttribute {
 
     public static final String SEPARATOR = "/";
     public static final String FOLDER_MIME_TYPE = "custom/folder";
-    public static final String DOWNLOAD_ZIP_SUFFIX = "-download.zip";
+    public static final String DOWNLOAD_ZIP_NAME = "download.zip";
     public static final String ZIP_MEDIA_TYPE = "application/zip";
 
     public enum Type {
