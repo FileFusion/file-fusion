e83d00e580d05c96e6da4b87f1960042b849749f
hackyo <137120918@qq.com>
2025-03-01 16:01:44 +0800

    file operate

---
+++ backend/src/main/java/com/github/filefusion/config/JacksonConfig.java
@@ -0,0 +1,53 @@
+package com.github.filefusion.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.github.filefusion.util.TimeUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
+/**
+ * JacksonConfig
+ *
+ * @author hackyo
+ * @since 2022/4/1
+ */
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        simpleModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+        return Jackson2ObjectMapperBuilder.json()
+                .timeZone(TimeZone.getTimeZone(TimeUtil.DEFAULT_ZONE))
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .modulesToInstall(simpleModule)
+                .build();
+    }
+
+    public static class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+        @Override
+        public void serialize(LocalDateTime localDateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeNumber(localDateTime.atZone(TimeUtil.DEFAULT_ZONE).toInstant().toEpochMilli());
+        }
+    }
+
+    public static class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+        @Override
+        public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            String value = jsonParser.getValueAsString();
+            return StringUtils.hasLength(value) ? TimeUtil.fromMillis(Long.parseLong(value)) : null;
+        }
+    }
+
+}
