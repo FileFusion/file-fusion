fbb6484113242031522dbd0319af6221b404a4e7
hackyo <137120918@qq.com>
2025-03-12 13:46:57 +0800

    update plugins

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,11 @@
 import 'virtual:uno.css';
 import '@/style.css';
-import type { Emitter } from 'mitt';
 import type { NotificationApi } from 'naive-ui';
 import type { DialogApi } from 'naive-ui';
 import type { LoadingBarApi } from 'naive-ui';
 import { createApp } from 'vue';
 import { createPinia, type Pinia } from 'pinia';
-import mitt from 'mitt';
+import EventEmitter from 'eventemitter3';
 import i18n from '@/commons/i18n';
 import msg from '@/commons/msg';
 import {
@@ -20,7 +19,7 @@ import App from '@/App.vue';
 declare global {
   interface Window {
     $pinia: Pinia;
-    $event: Emitter<any>;
+    $event: EventEmitter;
     $notice: NotificationApi;
     $dialog: DialogApi;
     $loading: LoadingBarApi;
@@ -33,7 +32,7 @@ declare global {
 const pinia: Pinia = createPinia();
 
 window.$pinia = pinia;
-window.$event = mitt<any>();
+window.$event = new EventEmitter();
 window.$msg = msg;
 window.$http = http;
 window.$t = i18n.global.t;
