de5e8f4d3ef2adaa5e7ee3e64111d1e576744abd
hackyo <137120918@qq.com>
2025-03-29 00:10:26 +0800

    update plugins

---
+++ backend/src/main/java/com/github/filefusion/config/ServerContextListener.java
@@ -0,0 +1,22 @@
+package com.github.filefusion.config;
+
+import com.github.filefusion.util.ExecUtil;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ServerContextListener
+ *
+ * @author hackyo
+ * @since 2022/4/1
+ */
+@Configuration
+public class ServerContextListener implements ServletContextListener {
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        ExecUtil.shutdown();
+    }
+
+}
